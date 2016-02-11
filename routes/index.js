e08eb3543bba26e7ca838a6e6f98594a8a72cc8a
Bleep-Bloop <TaisenColcher@Gmail.com>
2016-02-10 23:36:57 -0500

    "Fixed up form and some css changes"

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/about', function(req, res, next) {
 
 /* GET contact page */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact Mea ' });
+  res.render('contact', { title: 'Contact Me ' });
 });
 
 /* GET services page */
